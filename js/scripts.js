bea4012fb8419ddc489d41712769711080175406
Alan Bernal <abernalespinoza@outlook.com>
2020-10-14 22:57:40 -0500

    Version 1.0

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    var flag = 0;
+
+    $('[data-toggle="tooltip"]').tooltip()
+
+    $('#menu_hamburger').click(function () {
+        if(flag == 0) {
+            $('#sidebar-navigation').addClass('show')
+            $('.sidebar__overlay').addClass('show')
+            flag = 1
+        }
+    })
+
+    $('#icon_times').click(function () {
+        if(flag == 1) {
+            $('#sidebar-navigation').removeClass('show')
+            $('.sidebar__overlay').removeClass('show')
+            flag = 0;
+        }
+    })
+
+    $('.categories__item > a').click(function () {
+        $(this).next('ul').toggleClass('show');
+    })
+
+    $('.categories__subitem > a').click(function () {
+        $(this).next('ul').toggleClass('show');
+    })
+})
